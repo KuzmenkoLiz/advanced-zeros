fa501964c8e1d90acf4347e79ee063d62bf7f171
KuzmenkoLiz <lizaveta330@gmail.com>
2018-02-26 02:25:44 +0300

    COME ON LIZ! Still need right version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-  /**/
+  
   var zerosCount = 0;
+  /*var first = 2;
+
+  while (first <= base && base <= 256) {
+    if (base%first == 0) {
+      base = Math.floor(base/first);
+    } else {
+      first++;
+    }
+  }*/
   
   while(number){
-    for (var i = 0; i < base; i++) {
+    for (var i = 1; i <= base/2; i++) {
       number = Math.floor(number/Math.pow(5, i));
       zerosCount += number;
     }
