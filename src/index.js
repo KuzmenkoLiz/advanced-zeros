4926c22354ccffbb8aa0807ce5e7ecd36ac7a0a4
KuzmenkoLiz <lizaveta330@gmail.com>
2018-02-26 02:10:48 +0300

    NEED right version BC ITS WRONG

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  /**/
+  var zerosCount = 0;
+  
+  for (var i = 0; i < base; i++) {
+    numbe = Math.floor(number/Math.pow(num, i));
+    zerosCount += numbe;
+  }
+  
+  return zerosCount;
+
 }
